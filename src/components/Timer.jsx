9b6452b01c923989b4f8a8e50c99ccab7036fea4
Nasr Galal <nasrbue@gmail.com>
2019-12-04 11:47:33 +0200

    fixed css -- added copyright

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 let countdown;
 
 class Timer extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       timer: `00:00`,
       endTime: `00:00`,
       // seconds: 0,
       // minutes: 0,
       inputMinutes: '',
-      chosenValue: 'break'
-    };
+      chosenValue: 'break',
+      coretabs: 'https://forums.coretabs.net/t/سلسلة-التحدي-الأسبوعي-التحدي-3/3578/12'
+    }
 
-    this.startSound = React.createRef();
-    this.beebSound = React.createRef();
+    this.startSound = React.createRef()
+    this.beebSound = React.createRef()
   }
 
   handleSelect = (event) => {
@@ -27,23 +28,23 @@ class Timer extends Component {
   reset = () => {
     const { responsiveVoice } = window
     this.timeStop(); // stopping js counting
-    this.setState({ timer: `00:00` });
-    responsiveVoice.cancel();
+    this.setState({ timer: `00:00` })
+    responsiveVoice.cancel()
   }
 
   timeStop = () => {
-    clearInterval(countdown);
+    clearInterval(countdown)
   }
 
   displayTimeLeft = (seconds) => {
     const { responsiveVoice } = window
-    const minutes = Math.floor(seconds / 60);
-    const secondsLeft = seconds % 60;
+    const minutes = Math.floor(seconds / 60)
+    const secondsLeft = seconds % 60
     this.setState({
       timer: `${minutes < 10 ? 0 : ""}${minutes}:${
         secondsLeft < 10 ? 0 : ""
       }${secondsLeft}`
-    });
+    })
 
     // ***************** UPDATED *********************
     // voice alarm for each minute
@@ -51,46 +52,46 @@ class Timer extends Component {
     if (secondsLeft === 0 && minutes !== 0) {
       responsiveVoice.speak(`${minutes} minutes remaining | till ${this.state.chosenValue} ends`);
     } else if (secondsLeft === 20 && minutes === 0) {
-      responsiveVoice.speak(`20 seconds remaining`);
+      responsiveVoice.speak(`20 seconds remaining`)
     } else if (minutes === 0 && secondsLeft === 0) {
       responsiveVoice.speak(
         `Your Break Has Finished. Hurry up before you get fired! Thank you for using our services`
       );
     } else if (minutes === 0 && secondsLeft < 11) {
-      responsiveVoice.speak(`${secondsLeft}`);
+      responsiveVoice.speak(`${secondsLeft}`)
     }
   }
   displayEndTime = (timestamp) => {
     console.log('endtime')
-    const end = new Date(timestamp);
-    const hour = end.getHours();
-    const CorrHours = hour > 12 ? hour - 12 : hour;
-    const minutes = end.getMinutes();
+    const end = new Date(timestamp)
+    const hour = end.getHours()
+    const CorrHours = hour > 12 ? hour - 12 : hour
+    const minutes = end.getMinutes()
     this.setState({
       endTime: `Break ends at ${CorrHours < 10 ? 0 : ""}${CorrHours}:${
         minutes < 10 ? 0 : ""
       }${minutes}`
-    });
+    })
   }
   startTimer = (x) => {
     this.reset();
     this.startSound.current.play();
-    const targetSec = parseInt(x.target.dataset.time);
-    console.log(targetSec);
-    this.timer(targetSec);
+    const targetSec = parseInt(x.target.dataset.time)
+    console.log(targetSec)
+    this.timer(targetSec)
   }
 
   // timer mechanics
   timer = (seconds) => {
     const { responsiveVoice } = window
-    this.reset();
+    this.reset()
 
     // constants
 
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    this.displayTimeLeft(seconds);
-    this.displayEndTime(then);
+    const now = Date.now()
+    const then = now + seconds * 1000
+    this.displayTimeLeft(seconds)
+    this.displayEndTime(then)
 
     // setting animation in one second and storing it in a variable (countdown)
     // to be able to clearInterval after it reaches zero
@@ -109,9 +110,9 @@ class Timer extends Component {
 
       if (remainedSeconds < 11) {
         for (let sec in remainedSeconds) {
-          responsiveVoice.speak(`${sec}`);
+          responsiveVoice.speak(`${sec}`)
         }
-        this.beebSound.current.play();
+        this.beebSound.current.play()
       }
 
       // changing colors once reaching 10 seconds (needs fixing!)
@@ -120,22 +121,24 @@ class Timer extends Component {
       }
 
       // Displaying timer
-      this.displayTimeLeft(remainedSeconds);
-    }, 1000);
+      this.displayTimeLeft(remainedSeconds)
+    }, 1000)
   }
   submitMins = (e) => {
-    e.preventDefault();
-    this.reset();
-    this.setState({ inputMinutes: e.target.value });
-    const mins = this.enteredMins;
-    this.timer(mins * 60);
-    this.setState({ timer: `${mins}` });
+    e.preventDefault()
+    this.reset()
+    this.setState({ inputMinutes: e.target.value })
+    const mins = this.enteredMins
+    this.timer(mins * 60)
+    this.setState({ timer: `${mins}` })
   }
 
   render() {
     return (
       <div className="overlay">
         <div className="timer">
+        <h3>This material is programmed by &copy; 2019 Nasr Galal</h3>
+        <h3>content revised and authorized by <a href={this.state.coretabs}>Coretabs Academy</a></h3>
           <div className="thanks">
             <h3>ReactJS Stop watch</h3>
             <h5>Special thanks to: </h5>
@@ -180,7 +183,7 @@ class Timer extends Component {
               data-time="3600"
               className="timer__button"
             >
-              Lunch Break
+              Long {this.state.chosenValue}
             </button>
 
             <form onSubmit={this.submitMins} name="customForm">
@@ -208,6 +211,7 @@ class Timer extends Component {
               Mission launch at {this.state.endTime}
             </p>
           </div>
+          <h3>This material is programmed by &copy; 2019 Nasr Galal</h3>
         </div>
 
         <audio
